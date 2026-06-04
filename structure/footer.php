6b5f3732aa346bce3d210110073cbae70c035ea4
admin12place <thierry@randovelo34.fr>
2026-06-04 20:14:12 +0200

    mise en place du widget tripadvisor

--- structure/footer.php
+++ structure/footer.php
@@ -2,7 +2,7 @@
     <article class="site-description">
         <div>
             <img class="logo-left" src="./images/logo-dlsp-alpha.webp" alt="Logo Des lits sur la place"/>
-            <p><?php echo $footerText; ?></p>
+            <script async src="https://www.jscache.com/wejs?wtype=rated&amp;uniq=940&amp;locationId=1907792&amp;lang=fr&amp;display_version=2" data-loadtrk onload="this.loadtrk=true"></script>
         </div>
     </article>
 
@@ -10,18 +10,19 @@
             <span><a target="_blank" href="https://www.parc-haut-languedoc.fr/"><img src="./assets/pnrhl-icon.webp" alt="Parc naturel Régional du Haut-Languedoc"></a></span>
             <span><a target="_blank" href="https://www.gites-de-france.com/fr/occitanie/herault/des-lits-sur-la-place-34g11941?from=2026-06-18&to=2026-06-19&adults=2&children=0&infants=0"><img src="./assets/gdf-icon.webp" alt="Gîtes de France"></a></span>
             <span><a target="_blank" href="https://www.francevelotourisme.com/hebergements?ville=H%C3%A9r%C3%A9pian%20%2834600%29&villeid=FR34119"><img src="./assets/accueil_velo_icon.webp"alt="Label Accueil Vélo"></a></span>
-    </article>
+            <span><?php include 'templates/tripadvisorLink.php'; ?></span>
+        </article>
 
     <article class="main-menu-footer">
         <?php require 'structure/main-menu.php'; ?>
     </article>
 </div>
 
-<span class="copyright">
+<span class="copyright"><!--Le sigle copyright en debut de balise-->
     <span>&#xA9; des lits sur la place 2026. tous droits réservés</span>
     <a href="#">Mentions légales</a>
     <a href="/policy.php">Politique de confidentialité</a>
-</span><!--Le sigle cpoyright en debut de balise-->
+</span>
 
 <a href="#header-type" class="back-menu" title="Haut de page"><i class="fa-solid fa-arrow-up"></i></a>
         
